a4e99f47e3d0d164450ed02e9666ad24c84f18d6
Bavfalcon9 <olybear9@gmail.com>
2022-11-07 19:58:52 -0600

    chore: More unique snowflakes

--- src/generator.rs
+++ src/generator.rs
@@ -1,6 +1,6 @@
-use std::process;
-
 use crate::snowflake::Snowflake;
+use std::process;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 /// This is a generation pool that will generate snowflakes.
 pub struct Pool {
@@ -8,18 +8,30 @@ pub struct Pool {
     worker_id: u64,
     /// The current increment of this pool.
     increment: u64,
+    /// The startup time of the pool. This is used with the process id
+    /// to make it unique
+    startup: u128,
 }
 
 impl Pool {
     pub fn new(worker_id: u64) -> Self {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_nanos();
         Self {
             worker_id: worker_id,
             increment: 0,
+            startup: now,
         }
     }
 
     pub fn generate(&mut self) -> Snowflake {
         self.increment += 1;
-        Snowflake::create(self.worker_id.into(), process::id().into(), self.increment.into())
+        Snowflake::create(
+            self.worker_id.into(),
+            self.startup as u64 - process::id() as u64,
+            self.increment.into(),
+        )
     }
 }
--- tests/generate.rs
+++ tests/generate.rs
@@ -1,3 +1,5 @@
+use std::{time::Duration, thread};
+
 use snowflake::generator::Pool;
 
 #[test]
@@ -10,4 +12,28 @@ fn test_pool() {
 
     println!("{:?}", snowflake.get_id());
     println!("{:?}", snowflake);
+}
+
+#[test]
+fn unique_generations() {
+    let mut pool = Pool::new(3);
+    thread::sleep(Duration::from_millis(200));
+    let mut pool2 = Pool::new(3);
+
+    let batch_one = (
+        pool.generate(),
+        pool.generate(),
+        pool.generate()
+    );
+
+    let batch_two = (
+        pool2.generate(),
+        pool2.generate(),
+        pool2.generate()
+    );
+
+    assert_ne!(batch_one.0.get_id(), batch_two.0.get_id());
+    assert_ne!(batch_one.1.get_id(), batch_two.1.get_id());
+    assert_ne!(batch_one.2.get_id(), batch_two.2.get_id());
+
 }
